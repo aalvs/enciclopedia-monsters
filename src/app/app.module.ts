96239b6e14817b120517c1e039c51b955dbe41a1
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-14 13:08:04 -0300

    Importado o módulo httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { MonstrosComponent } from './components/monstros/monstros.component';
+import { LideresComponent } from './components/lideres/lideres.component';
+import { MedalhasComponent } from './components/medalhas/medalhas.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiResourceService } from './shared/services/api-resource.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MonstrosComponent,
+    LideresComponent,
+    MedalhasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiResourceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
