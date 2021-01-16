d2c4830064a731a0d13da7d0b613c1ebdb951807
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-16 17:27:36 -0300

    Finalizado metodo get do service para requisição

--- src/app/shared/services/api-resource.service.ts
+++ src/app/shared/services/api-resource.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Monstros } from '../models/monstros.model';
+import { Lideres } from '../models/lideres.model';
+import { Medalhas } from '../models/medalhas.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +14,16 @@ export class ApiResourceService {
   apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
+
+  public getMontstros(): Observable<Monstros> {
+    return this.http.get<Monstros>(`${this.apiUrl}monstros`);
+  }
+
+  public getLideres(): Observable<Lideres> {
+    return this.http.get<Lideres>(`${this.apiUrl}lideres`);
+  }
+
+  public getMedalhas(): Observable<Medalhas> {
+    return this.http.get<Medalhas>(`${this.apiUrl}medalhas`);
+  }
 }
