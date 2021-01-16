fe37ff4397f18b85b3b97f618b725ca2f7d37a63
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-16 12:30:26 -0300

    Configurado rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LideresComponent } from './components/lideres/lideres.component';
+import { MedalhasComponent } from './components/medalhas/medalhas.component';
+import { MonstrosComponent } from './components/monstros/monstros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'monstros', component: MonstrosComponent },
+  { path: 'lideres', component: LideresComponent },
+  { path: 'medalhas', component: MedalhasComponent },
+  { path: '', redirectTo: '/monstros', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
