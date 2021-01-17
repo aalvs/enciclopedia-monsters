5f84ac69027679bc3c45b695eafd6c0e34afd62a
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-16 21:30:05 -0300

    Finalizado component medalhas

--- src/app/components/medalhas/medalhas.component.ts
+++ src/app/components/medalhas/medalhas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Medalhas } from 'src/app/shared/models/medalhas.model';
+import { ApiResourceService } from 'src/app/shared/services/api-resource.service';
 
 @Component({
   selector: 'app-medalhas',
@@ -8,11 +9,18 @@ import { Medalhas } from 'src/app/shared/models/medalhas.model';
 })
 export class MedalhasComponent implements OnInit {
 
-  medalhas: Medalhas[];
+  medalhas: Medalhas;
 
-  constructor() { }
+  constructor(public service: ApiResourceService) { }
 
   ngOnInit(): void {
+    this.listar();
   }
 
+  public listar() {
+    this.service.getMedalhas().subscribe(data => {
+      this.medalhas = data;
+      console.log(this.medalhas);
+    });
+  }
 }
