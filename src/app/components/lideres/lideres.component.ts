815f55377791e1b601d3bf9780ef81af8ed0ab05
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-01-16 21:29:41 -0300

    Finalizado component lideres

--- src/app/components/lideres/lideres.component.ts
+++ src/app/components/lideres/lideres.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Lideres } from 'src/app/shared/models/lideres.model';
+import { ApiResourceService } from 'src/app/shared/services/api-resource.service';
 
 @Component({
   selector: 'app-lideres',
@@ -8,11 +9,18 @@ import { Lideres } from 'src/app/shared/models/lideres.model';
 })
 export class LideresComponent implements OnInit {
 
-  lideres: Lideres[];
+  lideres: Lideres;
 
-  constructor() { }
+  constructor(public service: ApiResourceService) { }
 
   ngOnInit(): void {
+    this.listar();
   }
 
+  public listar() {
+    this.service.getLideres().subscribe(data => {
+      this.lideres = data;
+      console.log(this.lideres);
+    });
+  }
 }
